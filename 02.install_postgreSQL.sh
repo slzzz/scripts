2825b255d02e4c550dea6adf151d18e50f13da9f
slzzz <shenhaowee@gmail.com>
2020-11-09 18:00:39 +0800

    Update 02.install_postgreSQL.sh

--- 02.install_postgreSQL.sh
+++ 02.install_postgreSQL.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+function default(){
+  sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main"
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+  sudo apt-get update
+  sudo apt-get install postgresql-9.6
+}
+
+default
+sudo passwd postgres
